6f770f9f69639d1d758e729fe1b62a71d40a146e
Клеймёнов_ЮВ <Kleymenov.93@mail.ru>
2026-05-21 20:08:51 +0400

    добавление в Task_21052026 !!@#@#Git

--- src/main/java/example/Main1.java
+++ src/main/java/example/Main1.java
@@ -3,5 +3,6 @@ package example;
 public class Main1 {
     public static void main(String[] args) {
         System.out.println("Здравствуйте ребята");
+        System.out.println("dhgd");
     }
 }
