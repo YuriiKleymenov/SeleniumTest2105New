8cb82ed24c613c9f7e0f0df0e6b761f36421e189
Клеймёнов_ЮВ <Kleymenov.93@mail.ru>
2026-05-21 19:38:29 +0400

    добавление в Task_2105 Git

--- src/main/java/example/Main1.java
+++ src/main/java/example/Main1.java
@@ -3,5 +3,7 @@ package example;
 public class Main1 {
     public static void main(String[] args) {
         System.out.println("Здравствуйте ребята");
+
+        System.out.println("Java!");
     }
 }
